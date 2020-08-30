06190e2132b8513a8a77b5e8ad29d4ec92be03f5
christopherturnbull <cturnbull27995@gmail.com>
2020-08-30 18:25:54 +0100

    Added build and bdist

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='choomah-exception',
       version='0.1',
@@ -6,5 +6,31 @@ setup(name='choomah-exception',
       url='https://github.com/christopher-turnbull/choomah',
       author='Christopher Turnbull',
       author_email='cturnbull27995@gmail.com',
-      packages=['choomah'],
-      zip_safe=False)
+      packages=find_packages(),
+      zip_safe=False,
+      python_requires='>=3.6'
+      )
+
+
+# import setuptools
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+#
+# setuptools.setup(
+#     name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+#     version="0.0.1",
+#     author="Example Author",
+#     author_email="author@example.com",
+#     description="A small example package",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/pypa/sampleproject",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+# ,
+# )
