a5dd089d87f770824c16168b288d35eb0359b3d4
christopherturnbull <cturnbull27995@gmail.com>
2020-08-30 17:35:49 +0100

    Yenaye

--- __init__.py
+++ __init__.py
@@ -7,26 +7,27 @@ import wave
 #define stream chunk   
 chunk = 1024  
 
-#open a wav format music  
-f = wave.open(r"choomah.wav","rb")
-#instantiate PyAudio  
-p = pyaudio.PyAudio()  
-#open stream  
-stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-                channels = f.getnchannels(),  
-                rate = f.getframerate(),  
-                output = True)  
-#read data  
-data = f.readframes(chunk)  
+def choomah_scream():
+    #open a wav format music
+    f = wave.open(r"choomah.wav","rb")
+    #instantiate PyAudio
+    p = pyaudio.PyAudio()
+    #open stream
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
+                    channels = f.getnchannels(),
+                    rate = f.getframerate(),
+                    output = True)
+    #read data
+    data = f.readframes(chunk)
 
-#play stream  
-while data:  
-    stream.write(data)  
-    data = f.readframes(chunk)  
+    #play stream
+    while data:
+        stream.write(data)
+        data = f.readframes(chunk)
 
-#stop stream  
-stream.stop_stream()  
-stream.close()  
+    #stop stream
+    stream.stop_stream()
+    stream.close()
 
-#close PyAudio  
-p.terminate() 
+    #close PyAudio
+    p.terminate()
