28c5c96ef8c9a96e7230eb8fb115fd09e9f45e9b
christopherturnbull <cturnbull27995@gmail.com>
2020-08-30 17:32:23 +0100

    Yenaye

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='choomah-exception',
+      version='0.1',
+      description='- The playing of the said "choomah scream" everytime an exception is captured (similar to oooOOOOAAAAAAAAAAA )',
+      url='https://github.com/christopher-turnbull/Choomah',
+      author='Christopher Turnbull',
+      author_email='cturnbull27995@gmail.com',
+      packages=['chommah-exception'],
+      zip_safe=False)
